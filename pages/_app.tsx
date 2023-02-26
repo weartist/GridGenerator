a19c7f03d84b6818dba7b086616d643f49c73d29
weartist <weartistt@gmail.com>
2023-02-26 20:44:39 +0800

    add the grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import '@/styles/globals.css'
+import '@/styles/card.css'
+
+import '@/styles/music.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
