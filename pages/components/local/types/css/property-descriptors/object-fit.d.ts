2759cd695604246134df8869d8f4f86f860fc51a
weartist <weartistt@gmail.com>
2023-03-31 23:42:44 +0800

    💄

--- pages/components/local/types/css/property-descriptors/object-fit.d.ts
+++
@@ -1,9 +0,0 @@
-import { IPropertyIdentValueDescriptor } from '../IPropertyDescriptor';
-export declare const enum OBJECT_FIT {
-    FILL = "fill",
-    CONTAIN = "contain",
-    COVER = "cover",
-    NONE = "none",
-    SCALE_DOWN = "scale-down"
-}
-export declare const objectFit: IPropertyIdentValueDescriptor<OBJECT_FIT>;
--- pages/components/local/types/css/property-descriptors/object-position.d.ts
+++
@@ -1,4 +0,0 @@
-import { IPropertyListDescriptor } from '../IPropertyDescriptor';
-import { LengthPercentageTuple } from '../types/length-percentage';
-export declare type ObjectPosition = LengthPercentageTuple;
-export declare const objectPosition: IPropertyListDescriptor<ObjectPosition>;
